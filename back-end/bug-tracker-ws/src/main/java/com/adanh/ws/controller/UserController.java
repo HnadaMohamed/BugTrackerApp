83a191dcf112167ef46cd4a6ffaa9a535a83c215
HnadaSP <hnadamohamed18@gmail.com>
2021-12-26 17:54:20 +0100

    V2

--- back-end/bug-tracker-ws/src/main/java/com/adanh/ws/controller/UserController.java
+++ back-end/bug-tracker-ws/src/main/java/com/adanh/ws/controller/UserController.java
@@ -38,6 +38,6 @@ public class UserController {
 
 	@GetMapping("/welcome")
 	public String index() {
-	    return "Welcome jenkins auto deploy";
+	    return "Welcome jenkins auto deploy V2";
 	}
 }
